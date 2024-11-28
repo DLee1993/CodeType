d3973e83474f80cf0821b688a4bf31b9771e59b4
David Lee <Dai180293@gmail.com>
2024-11-28 13:44:17 +0000

    added: refresh test button styling and jsx

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,39 @@ export default function Home() {
             </header>
             <main className="w-11/12 max-w-5xl mx-auto flex flex-col gap-10 items-center">
                 <p className="text-xl min-h-20 leading-relaxed">{testContent}</p>
-                <p>refresh icon here</p>
+                {testContent && (
+                    <button className="group relative">
+                        <svg
+                            className="size-5"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g
+                                id="SVGRepo_tracerCarrier"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
+                                {" "}
+                                <path
+                                    d="M21 3V8M21 8H16M21 8L18 5.29168C16.4077 3.86656 14.3051 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.2832 21 19.8675 18.008 20.777 14"
+                                    stroke="#000000"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                ></path>
+                            </g>
+                        </svg>
+                        <div className="absolute z-10 top-8 left-1/2 -translate-x-1/2 w-40 inline-block bg-background shadow-sm text-foreground py-2 px-2 text-sm rounded-lg invisible group-hover:visible">
+                            restart the test
+                            <div className=" w-6 overflow-hidden absolute -top-3 left-1/2 -translate-x-1/2">
+                                <div className=" h-4 w-4 bg-background rotate-45 transform origin-bottom-left"></div>
+                            </div>
+                        </div>
+                    </button>
+                )}
             </main>
             <footer className="min-h-20 flex justify-between items-center px-5 sm:px-10 py-5 sm:py-0">
                 {/* when clicked, fetch a new word | quote | codeblock */}
