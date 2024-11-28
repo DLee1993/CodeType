ddb11c85d4677efd6242b0a5673f437c72595a19
David Lee <Dai180293@gmail.com>
2024-11-28 12:45:24 +0000

    added fetch functions

--- services/localFunctions.ts
+++ services/localFunctions.ts
@@ -1,19 +1,76 @@
-//? Functions to be called
+//* GET RANDOM ITEM FROM DATA
 
-export const fetchCode = async () => {
-    const res = await fetch("./static/codeBlocks/english.json");
-    const data = await res.json();
-    console.log(data);
+function getRandomQuote(array: { id: number; string: string }[]) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex].string;
+}
+
+//? FETCH
+
+export const fetchCode = async (length: number) => {
+    try {
+        const response = await fetch("./static/codeBlocks/english.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        const filteredData = data["content"].filter((item: { id: string; string: string }) => {
+            const wordsInString = item.string.split(" ");
+            return wordsInString.length <= length;
+        });
+
+        const random = getRandomQuote(filteredData);
+
+        return random;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 };
 
-export const fetchQuotes = async () => {
-    const res = await fetch("./static/quotes/english.json");
-    const data = await res.json();
-    console.log(data);
+export const fetchQuotes = async (length: number) => {
+    try {
+        const response = await fetch("./static/quotes/english.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        const filteredData = data["content"].filter((item: { id: string; string: string }) => {
+            const wordsInString = item.string.split(" ");
+            return wordsInString.length <= length;
+        });
+
+        const random = getRandomQuote(filteredData);
+
+        return random;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 };
 
-export const fetchWords = async () => {
-    const res = await fetch("./static/words/english.json");
-    const data = await res.json();
-    console.log(data);
+export const fetchWords = async (length: number) => {
+    const words: string[] = [];
+
+    try {
+        const response = await fetch("./static/words/english.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        while (words.length <= length - 1) {
+            const randomIndex = Math.floor(Math.random() * data["content"].length);
+            const num = data["content"][randomIndex];
+            if (!words.includes(num)) {
+                words.push(num);
+            }
+        }
+
+        const sentence = words.join(",").replaceAll(",", " ");
+
+        return sentence;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 };
