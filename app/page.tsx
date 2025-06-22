9087996bc36ba982b68be03e33df745a4ec8d596
David Lee <Dai180293@gmail.com>
2025-06-22 11:26:29 +0100

    updated page to now import Settings component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { fetchCode, fetchQuotes, fetchWords } from "@/services/localFunctions";
-import Link from "next/link";
 import TypeThroughInput from "@/components/TypeThroughInput";
-import ThemeSelector from "@/components/ThemeSelector";
-import HelpGuide from "@/components/HelpGuide";
+import Settings from "@/components/Settings";
 
 export default function Home() {
     const [testType, setTestType] = useState<string>();
@@ -14,16 +12,6 @@ export default function Home() {
     const [generateNewTest, setGenerateNewTest] = useState<boolean>();
 
     const refreshText = () => setGenerateNewTest(true);
-
-    const changeTestType = (string: string) => {
-        setTestType(string);
-        localStorage.setItem("testType", string);
-    };
-
-    const changeTestLength = (newLength: number[]) => {
-        setTestLength([Number(newLength[0]), Number(newLength[1])]);
-        localStorage.setItem("testLength", newLength.toString());
-    };
 
     //# Get the stored args and add the transition classes after mounting
     useEffect(() => {
@@ -77,88 +65,16 @@ export default function Home() {
 
     return (
         <>
-            <header className="min-h-20 flex flex-wrap justify-between items-center p-5 gap-y-5">
+            <header className="h-20 flex flex-wrap justify-between items-center p-5 gap-y-5">
                 <div>
                     <h1 className="text-lg font-semibold">Code-type</h1>
                     <sup>A minimalistic typing test</sup>
                 </div>
-                <div className="w-full max-w-sm sm:w-auto flex justify-between items-center gap-x-4">
-                    <ul
-                        className={`flex justify-center items-center gap-x-4 ${
-                            testType === "words" ? "opacity-100" : "opacity-25 pointer-events-none"
-                        }`}
-                    >
-                        <li
-                            onClick={() => changeTestLength([1, 30])}
-                            className={`${
-                                testLength[1] === 30 && testType === "words" ? "underline text-accent" : "text-foreground"
-                            } cursor-pointer`}
-                        >
-                            30
-                        </li>
-                        <li
-                            onClick={() => changeTestLength([31, 60])}
-                            className={`${
-                                testLength[1] === 60 && testType === "words" ? "underline text-accent" : "text-foreground"
-                            } cursor-pointer`}
-                        >
-                            60
-                        </li>
-                        <li
-                            onClick={() => changeTestLength([61, 120])}
-                            className={`${
-                                testLength[1] === 120 && testType === "words" ? "underline text-accent" : "text-foreground"
-                            } cursor-pointer`}
-                        >
-                            120
-                        </li>
-                    </ul>
-                    <div className="w-[1px] h-5 bg-foreground"></div>
-                    <ul className="flex justify-center items-center gap-x-4">
-                        <li
-                            onClick={(e) => changeTestType(e.currentTarget.innerHTML)}
-                            className={`${
-                                testType === "words" ? "underline text-accent" : "text-foreground"
-                            } cursor-pointer`}
-                        >
-                            words
-                        </li>
-                        <li
-                            onClick={(e) => changeTestType(e.currentTarget.innerHTML)}
-                            className={`${
-                                testType === "quotes" ? "underline text-accent" : "text-foreground"
-                            } cursor-pointer`}
-                        >
-                            quotes
-                        </li>
-                        <li
-                            onClick={(e) => changeTestType(e.currentTarget.innerHTML)}
-                            className={`${
-                                testType === "code" ? "underline text-accent" : "text-foreground"
-                            } cursor-pointer`}
-                        >
-                            code
-                        </li>
-                    </ul>
-                </div>
+                <Settings testType={testType} testLength={testLength} setTestType={setTestType} setTestLength={setTestLength} />
             </header>
-            <main>
+            <main className="h-[calc(100vh-150px)] grid place-content-center">
                 <TypeThroughInput text={testContent!} refreshText={refreshText} />
             </main>
-            <footer className="min-h-20 flex justify-between items-center px-5">
-                <section className="flex justify-center items-center gap-2">
-                    <HelpGuide />
-                    <p>/</p>
-                    <ThemeSelector />
-                </section>
-                <Link
-                    href={"https://github.com/DLee1993/CodeType#codetype-guide"}
-                    target="_blank"
-                    className="hover:text-accent hover:transition-colors"
-                >
-                    github
-                </Link>
-            </footer>
         </>
     );
 }
